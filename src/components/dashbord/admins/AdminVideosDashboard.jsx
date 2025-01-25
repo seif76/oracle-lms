5d6f7cf469f4c9676596c82c544f4af148aebc5f
seif amir <seifamir76@gmail.com>
2025-01-25 23:56:14 +0200

    last commit on this branch

--- src/components/dashbord/admins/AdminVideosDashboard.jsx
+++ src/components/dashbord/admins/AdminVideosDashboard.jsx
@@ -131,7 +131,30 @@ const AdminVideosDashboard = () => {
       {isAddModalOpen && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
           <div className="bg-white p-6 rounded-lg w-[90%] max-w-md">
+          <div className="flex items-center justify-between mb-6">
             <h2 className="text-xl font-bold mb-4">Add New video</h2>
+            <button
+          onClick={() => setAddModalOpen(false)}
+          className="text-gray-500  hover:text-gray-800"
+          aria-label="Close"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+          
+        </button>
+        </div>
             <VideoCreationForm
               onClose={() => setAddModalOpen(false)}
               onVideoCreated={handlevideoCreated}
